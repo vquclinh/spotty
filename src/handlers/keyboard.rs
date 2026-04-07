3605d519532e7372cabfea3580dbc9704a832bff
vquclinh <voquoclinh.29042006@gmail.com>
2026-04-07 21:09:20 +0700

    add quit keyboard

--- src/handlers/keyboard.rs
+++ src/handlers/keyboard.rs
@@ -1,6 +1,21 @@
 use crate::app::App;
 use crate::event::Event;
+use crossterm::event::{KeyCode, KeyModifiers};
 
-pub fn handle(_event: Event, _app: &mut App) {
-    // TODO
+pub fn handle(event: Event, app: &mut App) {
+    if let Event::Key(key) = event {
+        match key.code {
+            KeyCode::Char('q') => {
+                app.should_quit = true;
+            }
+            KeyCode::Esc => {
+                app.should_quit = true;
+            }
+            KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                app.should_quit = true;
+            }
+
+            _ => {}
+        }
+    }
 }
